5acea7cacd45c3830ff28cea1313c505bd13ad8b
Amvenix-devly <globalcommunityorganization@gmail.com>
2025-10-15 01:25:35 +0600

    Update files with latest changes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,8 @@ export default function AboutPage() {
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h1>Amvenix Business World</h1>
-      <p>Welcome to our website! Amvenix Business World</p>
+      <p>Welcome to our website!</p>
+      <p>Cooming Soon</p>
     </div>
   );
 }
