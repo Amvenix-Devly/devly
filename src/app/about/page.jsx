47ee471accc9b13eeffbb401d340c126b06b1d90
Amvenix-devly <globalcommunityorganization@gmail.com>
2025-10-15 00:07:48 +0600

    Initial commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Amvenix-devly</h1>
+      <p>Welcome to our website! Amvenix Business World.</p>
+    </div>
+  );
+}
